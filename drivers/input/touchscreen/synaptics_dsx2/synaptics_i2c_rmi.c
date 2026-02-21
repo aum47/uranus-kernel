6feadccef0f48a5b20989e46b7c4c828d636cbc0
Andevoloper <yogurtbirsah65@gmail.com>
2026-02-21 23:09:28 +0300

    Revert "drivers: Disable runtime PM for synaptics touchscreen (synaptics_dsx2) and bcmdhd WiFi (bcmdhd_1_77)"
    Re-enable runtime PM for synaptics_dsx2 and bcmdhd_1_77
    
    Disabling runtime PM increased idle power consumption.
    Restore runtime power management.
    
    This reverts commit f4d0325178d091e8e6a47f2d91b448b5a83b2167.

--- drivers/input/touchscreen/synaptics_dsx2/synaptics_i2c_rmi.c
+++ drivers/input/touchscreen/synaptics_dsx2/synaptics_i2c_rmi.c
@@ -4541,8 +4541,8 @@ static int synaptics_rmi4_resume(struct device *dev)
 #endif
 
 static const struct dev_pm_ops synaptics_rmi4_dev_pm_ops = {
-	.suspend = NULL,
-	.resume  = NULL,
+	.suspend = synaptics_rmi4_suspend,
+	.resume  = synaptics_rmi4_resume,
 };
 #endif
 
